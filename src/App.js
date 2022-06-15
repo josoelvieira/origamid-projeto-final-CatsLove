45188f6c0a24cb987141cd4343812770a7a3b76d
Josoel Vieira <cvl.jv1@gmail.com>
2022-06-15 16:20:17 -0300

    criando header footer e lpag login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+
+
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-     test
+     <Header />
+     <Routes>
+      <Route path="/" element={<Home />} />
+     </Routes>
+     <Footer />
     </div>
+    </BrowserRouter>
   );
 }
 
