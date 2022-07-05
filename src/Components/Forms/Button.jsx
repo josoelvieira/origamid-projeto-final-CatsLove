78235c5128871bdb9bc9dfc428906964a823604f
Josoel Vieira <cvl.jv1@gmail.com>
2022-07-05 19:21:09 -0300

    criando hook useform

--- src/Components/Forms/Button.jsx
+++ src/Components/Forms/Button.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Button.module.css';
+import styles from '../Forms/Button.module.css';
 
 const Button = ({ children, ...props }) => {
   return (
--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
-import { useState } from 'react';
+import React from 'react';
 
-const useForm = () => {
-    const [value, setValue] = useState('')
+const types = {
+  email: {
+    regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Preencha um email válido',
+  },
+};
 
-    function onChange({target}){
-        setValue(target.value)
+const useForm = (type) => {
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(null);
+
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError('Preencha um valor.');
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
     }
-    
-  return ({
-   value,
-   setValue,
-   onChange,
-})
-}
+  }
 
-export default useForm
+  function onChange({ target }) {
+    if (error) validate(target.value);
+    setValue(target.value);
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+  };
+};
+
+export default useForm;
