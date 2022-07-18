0499026fbd34f0aa689658a5354bfa19ed6983f5
Josoel Vieira <cvl.jv1@gmail.com>
2022-07-18 19:45:20 -0300

    deixando mais robusto, UseContext

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styles from './Header.module.css'
-import  { ReactComponent as Dogs } from '../Assets/logo.svg'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+import { ReactComponent as Dogs } from "../Assets/logo.svg";
+import { UserContext } from "../UserContext";
 
 const Header = () => {
-  return (
-    <header className={styles.header}>
-        <nav className={`${styles.nav} container`}>
-            <Link className={styles.logo} to="/" aria-label="Cats Love - Home"><Dogs /></Link>
-            <Link  className={styles.login} to="/login">Login / Criar</Link>
-        </nav>
-    </header>
-  )
-}
+    const { data, userLogout } = useContext(UserContext);
+    return (
+        <header className={styles.header}>
+            <nav className={`${styles.nav} container`}>
+                <Link
+                    className={styles.logo}
+                    to="/"
+                    aria-label="Cats Love - Home"
+                >
+                    <Dogs />
+                </Link>
+                {data ? (
+                    <Link className={styles.login} to="/conta">
+                        {data.nome}
+                        <button onClick={userLogout}>Sair</button>
+                    </Link>
+                ) : (
+                    <Link className={styles.login} to="/login">
+                        Login / Criar
+                    </Link>
+                )}
+            </nav>
+        </header>
+    );
+};
 
-export default Header
+export default Header;
--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 import { createContext } from 'react'
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET} from './api'
 
 export const UserContext = createContext()
 
 export const UserStorage = ({ children }) => {
-  return <UserContext.Provider value ={{usuario}}>{children}</UserContext.Provider>
+  const [data, setData] = useState(null)
+  const [login, setLogin] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+
+  useEffect(() =>{
+    async function autoLogin() {
+      const token = window.localStorage.getItem('token')
+      if(token) {
+        try{
+          setError(null)
+          setLoading(true)
+        const {url, options} = TOKEN_VALIDATE_POST(token)
+        const response = await fetch(url, options)
+        if(!response.ok) throw new Error('Token invalido')
+        await getUser(token)
+      }catch(err){
+        userLogout()
+      }finally {
+        setLoading(false)
+      }
+      }}
+    autoLogin()
+  },[])
+async function getUser (token) {
+  const {url, options} = USER_GET(token)
+  const response = await fetch(url, options)
+  const json =  await response.json()
+  setData(json)
+  setLogin(true)
+
+}
+
+async function userLogin (username, password) {
+  try{
+    setError(null)
+    setLoading(true)
+  const {url, options} = TOKEN_POST({username, password})
+  const tokenRes = await fetch(url, options)
+  if(!tokenRes.ok) throw new Error(`Error: ${tokenRes.statusText}`)
+  const {token} = await tokenRes.json()
+  window.localStorage.setItem('token', token)
+  getUser(token)
+  } catch (err) {
+    setError(err.message)
+    setLogin(false)
+  } finally {
+    setLoading(false)
+  }
+}
+async function userLogout() {
+  setData(null)
+  setError(null)
+  setLoading(false)
+  setLogin(false)
+  window.localStorage.removeItem('token')
+}
+
+  return <UserContext.Provider value={{userLogin, data,userLogout}}>{children}</UserContext.Provider>
 }
