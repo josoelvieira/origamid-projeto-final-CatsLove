38ff33c76ea4eaea80418e68683cf76762fc8c2c
Josoel <cvl.jv1@gmail.com>
2022-07-21 22:05:12 -0300

    aplicando css ao login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,25 +1,28 @@
-import React, { useContext } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import LoginForm from './LoginForm'
-import LoginCriar from './LoginCriar'
-import LoginPerdeu from './LoginPerdeu'
-import LoginReset from './LoginResetar'
-import { UserContext } from '../../UserContext'
+import React, { useContext } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import LoginCriar from "./LoginCriar";
+import LoginPerdeu from "./LoginPerdeu";
+import LoginReset from "./LoginResetar";
+import { UserContext } from "../../UserContext";
+import styles from "./Login.module.css";
 
 const Login = () => {
-  const { login } = useContext(UserContext)
+    const { login } = useContext(UserContext);
 
-  if(login === true) return <Navigate to="/conta"/>
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm/>} />
-        <Route path="/criar" element={<LoginCriar/>} />
-        <Route path="/perdeu" element={<LoginPerdeu/>} />
-        <Route path="/resetar" element={<LoginReset/>} />
-      </Routes>
-    </div>
-  )
-}
+    if (login === true) return <Navigate to="/conta" />;
+    return (
+        <section className={styles.login}>
+            <div className={styles.forms}>
+                <Routes>
+                    <Route path="/" element={<LoginForm />} />
+                    <Route path="/criar" element={<LoginCriar />} />
+                    <Route path="/perdeu" element={<LoginPerdeu />} />
+                    <Route path="/resetar" element={<LoginReset />} />
+                </Routes>
+            </div>
+        </section>
+    );
+};
 
-export default Login
+export default Login;
