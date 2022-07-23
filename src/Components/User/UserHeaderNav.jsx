83c587108dc1539ff5e3804737a6ba730bc56a3a
Josoel <cvl.jv1@gmail.com>
2022-07-23 20:12:32 -0300

    criando page user

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { UserContext } from '../../UserContext';
-import { useContext } from 'react';
-import {ReactComponent as MinhasFotos } from "../../Assets/feed.svg";
-import {ReactComponent as Estats } from "../../Assets/estatisticas.svg";
-import {ReactComponent as EnviarAdd } from "../../Assets/enviar.svg";
-import {ReactComponent as Sair } from "../../Assets/sair.svg";
-import styles from './UserHeaderNav.module.css'
-import { useState } from 'react';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import { useContext } from "react";
+import { ReactComponent as MinhasFotos } from "../../Assets/feed.svg";
+import { ReactComponent as Estats } from "../../Assets/estatisticas.svg";
+import { ReactComponent as EnviarAdd } from "../../Assets/enviar.svg";
+import { ReactComponent as Sair } from "../../Assets/sair.svg";
+import styles from "./UserHeaderNav.module.css";
+import { useState } from "react";
 
 const UserHeaderNav = () => {
-    const [mobile, stMobile] = useState(null)
+    const [mobile, stMobile] = useState(null);
 
-    const { userLogout } = useContext(UserContext)
-  return (
-    <nav className={styles.nav}>
-        <NavLink to="/conta" end activeClassName={styles.active}><MinhasFotos/>
-        {mobile && "Minhas Fotos"}</NavLink>
-        <NavLink to="/conta/estatisticas" activeClassName={styles.active}><Estats/>{mobile && "Minhas estatisticas"}</NavLink>
-        <NavLink to="/conta/postar" activeClassName={styles.active}><EnviarAdd/>{mobile && "Enviar"}</NavLink>
-    <button className="stylesBtn" onClick={userLogout}><Sair/>{mobile && "Sair"}</button>
-    </nav>
-  )
-}
+    const { userLogout } = useContext(UserContext);
+    return (
+        <nav className={styles.nav}>
+            <NavLink to="/conta" end activeClassName={styles.active}>
+                <MinhasFotos />
+                {mobile && "Minhas Fotos"}
+            </NavLink>
+            <NavLink to="/conta/estatisticas" activeClassName={styles.active}>
+                <Estats />
+                {mobile && "Minhas estatisticas"}
+            </NavLink>
+            <NavLink to="/conta/postar" activeClassName={styles.active}>
+                <EnviarAdd />
+                {mobile && "Enviar"}
+            </NavLink>
+            <button className="stylesBtn" onClick={userLogout}>
+                <Sair />
+                {mobile && "Sair"}
+            </button>
+        </nav>
+    );
+};
 
-export default UserHeaderNav
+export default UserHeaderNav;
