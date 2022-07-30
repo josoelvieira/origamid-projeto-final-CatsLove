194231d240c67f26156f101a564a0210f0c448e4
Josoel <cvl.jv1@gmail.com>
2022-07-30 14:15:02 -0300

    resolvendo bug protectedroute

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext'
 import User from './Components/User/User';
-//import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import  from './Components/Helper/ProtectedRoute';
 
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
      <Routes>
       <Route path="/" element={<Home />} />
       <Route path="login/*" element={<Login />} />
-      <Route path="conta/*" element={<User />} />
+      <Route path="conta/*" element={<ProtectedRoute><User /></ProtectedRoute>} />
      </Routes>
      <Footer />
      </UserStorage>
